1cd016637558ca84c1fda29e304cf6fd496de48a
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2023-03-16 17:30:59 +0600

    update

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,14 +1,30 @@
+import AuthLogo from './extensions/logo.svg';
+import MenuLogo from './extensions/logo.svg';
+import favicon from './extensions/favicon.svg';
+
+
 const config = {
-  locales: [
-    'ru'
-  ],
+	auth: {
+		logo: AuthLogo,
+	},
+	head: {
+		favicon: favicon,
+	},
+	menu: {
+		logo: MenuLogo,
+	},
+	locales: [
+		'ru'
+	],
+	tutorials: false,
+	notifications: { releases: false },
 };
 
 const bootstrap = (app) => {
-  console.log(app);
+	console.log(app)
 };
 
 export default {
-  config,
-  bootstrap,
+	config,
+	bootstrap,
 };
